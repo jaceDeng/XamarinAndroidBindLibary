1aa772f5f434671ebda2a29b9aba4412c3e244cc
Dunn <micro.man@live.cn>
2018-03-31 11:24:07 +0800

    新曾bugly

---
+++ Bugly/BuglyDemo/BuglyDemo/DemoApplication.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BuglyDemo
+{
+    public class DemoApplication : Android.App.Application
+    {
+        public const string APP_ID = "请输入你的ID";
+        protected DemoApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+
+        }
+        public override void OnCreate()
+        { 
+            Com.Tencent.Bugly.Bugly.Init(this.ApplicationContext, APP_ID, false);
+            base.OnCreate();
+            throw new Exception("APP_ID = \"请输入你的ID\";");
+        }
+    }
+}
