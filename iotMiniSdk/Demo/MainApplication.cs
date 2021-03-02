5e6a290e9088669ddad999bd017d11211a039b3f
JSON <micro.man@live.cn>
2021-03-02 13:26:42 +0800

    支付宝iot设备

---
+++ iotMiniSdk/Demo/MainApplication.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Com.Alipay.Iot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demo
+{
+    public class MyAppliction : Application
+    {
+        public MyAppliction(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+
+        }
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            IotDefaultTinyApp.Init(this);
+        }
+    }
+}
